37af5df9e125a852ec1c33a8f09ddb18d7fc6b90
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-15 07:26:05 +0000

    added  comments

--- src/solution.py
+++ src/solution.py
@@ -1,4 +1,5 @@
 def sumadd():
+    #Ran in pycharm will run in codespaces at a later date
     # Have the input be processed as an integer since it is string by default
     num1 = int(input("Input your first value: "))
     num2 = int(input("Input your second value: "))
