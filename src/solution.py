adb819db17ca096e4ebf837e9430777e534a47f5
Jonash Gania <jonashtiglao@gmail.com>
2023-09-15 19:16:47 +0800

    sum of two numbers

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,7 @@
 def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+    return a + b
+
+
+print(add_numbers(10, -3))
+print(add_numbers(5, 7))
+print(add_numbers(-3, -4))
