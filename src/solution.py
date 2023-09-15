bf3c76ceeecc0a748afd4441ef38aecb1614875a
Cy12swe <andreisagum@gmail.com>
2023-09-15 23:25:43 +0800

    Update solution.py
    Completed the add_numbers() function. Run the test, all test is ok.
    
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,8 @@
 def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+    return a + b
+
+result = add_numbers(5,9)
+print(result)
+
+result1 = add_numbers(3,-8)
+print(result1)
