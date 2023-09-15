52769c5f0d1121f74189ae4e793c7927aee7b64a
ChristianJoyGaray <123853568+ChristianJoyGaray@users.noreply.github.com>
2023-09-15 22:29:33 +0800

    new

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,13 @@
 def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+    result = a + b
+    return result
+
+a = input("Enter the 1st integer to add: ")
+b = input("Enter the 2nd integer to add: ")
+
+a=int(a)
+b=int(b)
+
+sum_result = add_numbers(a, b)
+print("The sum of ",a , "and ",b , "is: " , sum_result)
+
