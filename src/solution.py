ae7ab5e1b65ddc22f965c4036faceb6f7acf5606
jlfx09 <ykiritsu@gmail.com>
2023-09-15 18:13:29 +0800

    Added solution.py
    Added solution.py and modified code to acquire two integers and get the summation of two integers afterwards
    
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,14 @@
-def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+def addtwonums():
+    
+    num1 = int(input("Please input the value of the first integer here: "))
+    num2 = int(input("Please input the value of the second integer here "))
+
+    # Place the formula for the operation here
+    sum = num1 + num2
+
+    #Printout the result of the summation of two numbers
+    print("The summation of the integers", num1, "and", num2, "is", sum)
+
+
+
+addtwonums()
