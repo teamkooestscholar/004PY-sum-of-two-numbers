910e66080cbf22008138b597065d52402d4e6c83
PlopN <jrmaliwat0424@gmail.com>
2023-09-15 23:26:14 +0800

    Inserted Code

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,19 @@
-def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+#Insert User Variables
+sum1 = input("Type In First Value: ")
+sum2 = input("Type In Second Value: ")
+
+#Computation
+sumtotal = float(sum1) + float(sum2)
+
+#Display
+#
+print('The sum of {0} and {1} is {2}'.format(sum1, sum2, sumtotal))
+
+#Possible Other Display Output
+#print(f"The sum of {sum1} and {sum2} is {sumtotal}")
+
+#Other Simplified Process In Doing This is:
+#sum1 = 1
+#sum2 = 2
+#add = sum1 + sum2
+#print(f"The Sum Is: {add}" )
