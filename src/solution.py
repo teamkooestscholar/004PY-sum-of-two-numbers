59b92ad5d09ab6f8988555e43672c4e352c190bf
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-15 07:20:36 +0000

    added solution

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,14 @@
-def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+def sumadd():
+    # Have the input be processed as an integer since it is string by default
+    num1 = int(input("Input your first value: "))
+    num2 = int(input("Input your second value: "))
+
+    # The equation
+    sum = num1 + num2
+
+    #the print statement
+    print("The Sum of ", num1, "and", num2, "is")
+    print(int(sum))
+
+
+sumadd()
