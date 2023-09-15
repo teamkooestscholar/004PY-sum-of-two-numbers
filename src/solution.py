5231f452f92e7e40e9123fcb222821277f69254e
Erhesvara <106751010+Mahesvaraaaa@users.noreply.github.com>
2023-09-15 21:59:00 +0800

    solution

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,22 @@
-def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+# Create a function that takes two numbers as arguments and return their sum.
+
+# Example 1.
+# Input: `add_numbers(5, 7)`
+# Output: `12`
+# Explanation: The sum of 5 and 7 is 12.
+
+# Example 2.
+# Input: `add_numbers(-3, -4)`
+# Output: `-7`
+# Explanation: The sum of -3 and -4 is -7.
+
+# Constrains: NONE
+
+def sum_two_numbers(num1, num2):
+    """ Return the sum of two values """
+    answer = num1 + num2
+    return answer
+
+user_num1 = int(float("first number: "))
+user_num2 = int(float("Second number: "))
+print(f"The sum of {user_num1} and {user_num2} is {sum_two_numbers(user_num1,user_num2)}")
