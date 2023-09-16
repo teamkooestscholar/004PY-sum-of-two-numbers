1e6c34a0a544d9efa6ff6c0a620ed7fc126ca42a
kellydeguzman <kellydeguzman15@gmail.com>
2023-09-16 22:23:25 +0800

    updated

--- src/solution.py
+++ src/solution.py
@@ -1,9 +1,3 @@
 def add_numbers(A, B):
     return A + B
 
-A = 7
-B = 3
-result = add_numbers(A, B)
-print(f"Input: add_numbers({A}, {B})")
-print(f"Output: {result}")
-print(f"Explanation: The sum of {A} and {B} is {result}.")
