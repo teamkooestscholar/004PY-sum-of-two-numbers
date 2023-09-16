ab3247d6a70265b330f5702e00f09a405ca0b56a
kellydeguzman <kellydeguzman15@gmail.com>
2023-09-16 22:02:43 +0800

    function added
    Co-authored-by: noeyislearning <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,9 @@
-def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+def add_numbers(A, B):
+    return A + B
+
+A = 7
+B = 3
+result = add_numbers(A, B)
+print(f"Input: add_numbers({A}, {B})")
+print(f"Output: {result}")
+print(f"Explanation: The sum of {A} and {B} is {result}.")
