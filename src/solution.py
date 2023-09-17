594b6966f82cf5b20cd03c8e83fa5fbd0febc124
Prgrmmr633 <airesmacalino09@gmail.com>
2023-09-17 15:45:34 +0800

    Co-authored-by: Noey Ignacio <hello@noeyislearning.dev> Co-authored-by: Noey Ignacio <noeyislearning@gmail.com>

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,11 @@
 def add_numbers(a, b):
     n = a + b
     return n
+
+sum1 = add_numbers(5,7)
+sum2 = add_numbers(-3,-4)
+sum3 = add_numbers(10,-3)
+
+print('The sum of 5 and 7 is: ',sum1)
+print('The sum of -3 and -4 is: ',sum2)
+print('The sum of 10 and -3 is: ',sum3)
