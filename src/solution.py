90bba4de19b36a38e547a48a26d7925b1bafe8c8
jlfx09 <ykiritsu@gmail.com>
2023-09-17 22:38:43 +0800

    Update Solution.py
    Removed Print for testing
    
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -3,6 +3,6 @@ def add_numbers(num1, num2):
     result = num1 + num2
 
     # Print the result
-    print("The summation of the integers", num1, "and", num2, "is", result)
+   
 
     return result
