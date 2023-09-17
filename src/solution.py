7b2adaaa448ea64ff7dfe674a7ad568e87f6571a
PlopN <jrmaliwat0424@gmail.com>
2023-09-17 19:49:39 +0800

    Change Of Computation
    Overdone and Overstated Instruction. Corrected My Mistake

--- src/solution.py
+++ src/solution.py
@@ -1,18 +1,24 @@
+def add_numbers(a,b):
+    result = a + b
+    return result
+
+
+#First Attempt Did Not Consult Test.py (My Bad)
 #Insert User Variables
-sum1 = input("Type In First Value: ")
-sum2 = input("Type In Second Value: ")
+#sum1 = input("Type In First Value: ")
+#sum2 = input("Type In Second Value: ")
 
 #Computation
-sumtotal = float(sum1) + float(sum2)
+#sumtotal = float(sum1) + float(sum2)
 
 #Display
 #.format is a fucntion use to add placeholder when printing a text to have a more professional print output.
-print('The sum of {0} and {1} is {2}'.format(sum1, sum2, sumtotal))
+#print('The sum of {0} and {1} is {2}'.format(sum1, sum2, sumtotal))
 
 #Possible Other Display Output
 #print(f"The sum of {sum1} and {sum2} is {sumtotal}")
 
-#Other Simplified Process In Doing This is:
+#Other Process In Doing This is:
 #sum1 = 1
 #sum2 = 2
 #add = sum1 + sum2
