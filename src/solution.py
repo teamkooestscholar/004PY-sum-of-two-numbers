e7038c7b70866bbb1ab5075548a3e9797c11e6b2
ysabelandreii <ysabeltapnio@gmail.com>
2023-09-17 14:22:45 +0800

    004py-sum-of-two-numbers
    Submission  of 004py - Sum of two numbers
    
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,20 @@
 def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+    return a + b
+
+# Example 1
+result1 = add_numbers(5, 7)
+print("Input: 5 and 7")  
+print("Output:", result1)
+print("Explanation: The sum of 5 and 7 is ", result1)
+
+# Example 2
+result2 = add_numbers(-3, -4)
+print("\nInput: -3 and -4")  
+print("Output:", result2)
+print("Explanation: The sum of -3 and -4 is ", result2)
+
+# Example 3
+result3 = add_numbers(10, -3)
+print("\nInput: 10 and -3")  
+print("Output:", result3)
+print("Explanation: The sum of 10 and -3 is ", result3)
