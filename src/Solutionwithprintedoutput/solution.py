d188f04b29c61929894b55f2c43549654675614c
Rory <82867552+cortezrc6@users.noreply.github.com>
2023-09-17 10:51:01 +0000

    Solution
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/Solutionwithprintedoutput/solution.py
+++ src/Solutionwithprintedoutput/solution.py
@@ -1,3 +1,5 @@
 def add_numbers(a, b):
     result = a + b
+
+    print("The sum of", a, "and", b, "is", result)
     return result
--- src/Solutionwithprintedoutput/test.py
+++ src/Solutionwithprintedoutput/test.py
@@ -5,17 +5,14 @@ class TestAddNumbers(unittest.TestCase):
     def test_add_positive_numbers(self):
         result = add_numbers(5, 7)
         self.assertEqual(result, 12)
-        print("The sum of 5 and 7 is",result)
 
     def test_add_negative_numbers(self):
         result = add_numbers(-3, -4)
         self.assertEqual(result, -7)
-        print("The sum of -3 and -4 is",result)
 
     def test_add_mixed_numbers(self):
         result = add_numbers(10, -3)
         self.assertEqual(result, 7)
-        print("The sum of 10 and -3 is",result)
 
 if __name__ == '__main__':
     unittest.main()
