fb6e5168e0746bd51dcc324a2d7ff4754f4f6da0
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-17 21:21:38 +0800

    update
    update

--- src/solution.py
+++ src/solution.py
@@ -1,15 +1,22 @@
-def sumadd():
-    #Ran in pycharm will run in codespaces at a later date
-    # Have the input be processed as an integer since it is string by default.
-    num1 = int(input("Input your first value: "))
-    num2 = int(input("Input your second value: "))
-
-    # The equation
-    sum = num1 + num2
-
-    #the print statement
-    print("The Sum of ", num1, "and", num2, "is")
-    print(int(sum))
 
 
-sumadd()
+
+# def sumadd(a,b):
+#     #Ran in pycharm will run in codespaces at a later date
+#     # Have the input be processed as an integer since it is string by default.
+#     # num1 = int(input("Input your first value: "))
+#     # num2 = int(input("Input your second value: "))
+
+#     # The equation
+#     sum = a + b
+
+#     #the print statement
+#     # print("The Sum of ", num1, "and", num2, "is")
+#     # print(int(sum))
+
+
+# sumadd(a,b)
+
+
+def sum_two_numbers(a, b):
+    return a + b
--- src/test.py
+++ src/test.py
@@ -1,17 +1,17 @@
 import unittest
-from solution import add_numbers
+from solution import sum_two_numbers
 
 class TestAddNumbers(unittest.TestCase):
     def test_add_positive_numbers(self):
-        result = add_numbers(5, 7)
+        result = sum_two_numbers(5, 7)
         self.assertEqual(result, 12)
 
     def test_add_negative_numbers(self):
-        result = add_numbers(-3, -4)
+        result = sum_two_numbers(-3, -4)
         self.assertEqual(result, -7)
 
     def test_add_mixed_numbers(self):
-        result = add_numbers(10, -3)
+        result = sum_two_numbers(10, -3)
         self.assertEqual(result, 7)
 
 if __name__ == '__main__':
