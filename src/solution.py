78cc530e92f68bd71c09635f45d4daa65597d919
Rory <82867552+cortezrc6@users.noreply.github.com>
2023-09-17 08:46:21 +0000

    Solution
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,19 @@
+#Sum-of-two-numbers
 def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+    result = a + b
+    return result
+
+#Example 1
+print("Example 1")
+sum_result = add_numbers(5, 7)
+print("The sum of 5 and 7 is" , sum_result,".")
+
+#Example 2
+print("\nExample 2")
+sum_result = add_numbers(-3, -4)
+print("The sum of -3 and -4 is" , sum_result,".")
+
+#Example 3
+print("\nExample 3")
+sum_result = add_numbers(10, -3)
+print("The sum of 10 and -3 is" , sum_result,".")
