9d10706d6374b983bded53355a53b0ab45ea9be9
Jayem0709 <123857306+Jayem0709@users.noreply.github.com>
2023-09-17 10:47:07 +0000

    Solution
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,14 +1,11 @@
-# def add_numbers(a, b):
-#     pass # Remove the function "pass" and write your code here
 
-# Define a function to add two numbers.
 def add_numbers(num1, num2):
-    # Calculate the sum of the two numbers.
+    
     result = num1 + num2
 
-    # Display a message with the sum.
+    
     print("The sum of", num1, "and", num2, "is equal to", result)
 
-    # Return the computed sum.
+  
     return result
 
