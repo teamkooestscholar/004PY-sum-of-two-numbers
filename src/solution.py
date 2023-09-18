8289262995ea5cd0a8d46cfaed80ceab851d8300
YoshiroSenpai <123640832+YoshiroTheDestroyer@users.noreply.github.com>
2023-09-18 09:16:19 +0800

    added a code

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,24 @@
-def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+def test_add_positive_numbers(num1, num2):
+  
+  return 5 + 7
+   result = test_add_positive_numbers (5, 7)
+   print (result)
+
+
+
+
+def test_add_negative_numbers(num1, num2)
+
+   return -3 + -4
+    result = test_add_negative_numbers
+    print (result)
+
+
+
+
+def test_add_mixed_numbers(num1, num2)
+
+  return 10 + -3
+   result = test_add_mixed_numbers(10, -3)
+   print (result)
+
