2c952cf9dedddb2e31c8b96c39fab75c953d0194
Tsukkiz <123831791+Tsukkiz@users.noreply.github.com>
2023-09-19 20:13:52 +0800

    PY4KENNETHMACARAYO

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,20 @@
-def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+def add_numbers():
+    while True:
+        num1_input = input("Enter the first number (or 'q' to quit): ")
+        if num1_input.lower() == 'q':
+            break
+        
+        num2_input = input("Enter the second number (or 'q' to quit): ")
+        if num2_input.lower() == 'q':
+            break
+        
+        try:
+            num1 = float(num1_input)
+            num2 = float(num2_input)
+            
+            sum_result = num1 + num2
+            print(f"The sum of {num1} and {num2} is: {sum_result}")
+        except ValueError:
+            print("Invalid input. Please enter valid numbers or 'q' to quit.")
+
+add_numbers()
