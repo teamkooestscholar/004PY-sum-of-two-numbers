82c54c182d79ecc18a0960797663a9f5d2549843
K-A-Elardo <142892729+K-A-Elardo@users.noreply.github.com>
2023-09-21 19:05:43 +0800

    yes

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,8 @@
-def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+def sum_two_numbers(num1, num2):
+    """ Return the sum of two values """
+    answer = num1 + num2
+    return answer
+
+user_num1 = int(float("first number: "))
+user_num2 = int(float("Second number: "))
+print(f"The sum of {user_num1} and {user_num2} is {sum_two_numbers(user_num1,user_num2)}")
