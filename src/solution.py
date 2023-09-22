883d13b219c15896beba917c5dd09fb42c8915c6
mitaka3 <123709415+mitaka3@users.noreply.github.com>
2023-09-22 17:28:50 +0800

    bituin sum

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,16 @@
-def add_numbers(a, b):
-    pass # Remove the function "pass" and write your code here
+def add_numbers(num1, num2):
+    """ 
+    This function calculates the sum of two numbers and returns the result.
+    """
+    sum_result = num1 + num2
+    return sum_result
+
+# Prompt the user to enter two numbers and store them as integers.
+user_num1 = int(input("Enter the first number: "))
+user_num2 = int(input("Enter the second number: "))
+
+# Calculate the sum of the user-provided numbers using the add_numbers function.
+result = add_numbers(user_num1, user_num2)
+
+# Display the sum to the user.
+print(f"The sum of {user_num1} and {user_num2} is {result}")
